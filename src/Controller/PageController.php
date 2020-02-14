e89dd615fd1c0868b92e2bbd8745515caf900e3d
Ibrahim Soro <coulibalysoroib@gmail.com>
2020-02-14 13:47:22 +0000

    Add HomepPage, AboutPage, HelpPage

---
+++ src/Controller/PageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="page.home")
+     */
+    public function index()
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/about", name="page.about")
+     */
+    public function about()
+    {
+        return $this->render("page/about.html.twig", []);
+    }
+
+    /**
+     * @Route("/help", name="page.help")
+     */
+    public function help()
+    {
+        return $this->render("page/help.html.twig", []);
+    }
+}
